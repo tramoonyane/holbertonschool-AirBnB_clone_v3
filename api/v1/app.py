2f7fe228ff74a92597642b32159e3e1c8cbc2384
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-21 00:54:29 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -30,6 +30,6 @@ def errorhandler(error):
 
 
 if __name__ == "__main__":
-    API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
-    API_PORT = getenv("HBNB_API_PORT", 5000)
-    app.run(host=API_HOST, port=API_PORT, threaded=True)
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", 5000)
+    app.run(host=host, port=port, threaded=True)
