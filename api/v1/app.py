c63376a68d28fb9c9fb0f2400d8a24eb29173e6d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-20 20:09:32 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,6 +20,6 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000'))
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
