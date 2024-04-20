3906dac0ab7eea136662e48f24163d0ef4a35468
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-20 17:44:53 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,11 @@
 #!/usr/bin/python3
-"""
-index
-"""
-
-from flask import jsonify
+# api/v1/views/index.py
 from api.v1.views import app_views
+from flask import jsonify
 
-from models import storage
-
-
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
-def status():
+@app_views.route('/status', methods=['GET'])
+def api_status():
     """
-    status route
-    :return: response with json
+    Returns the status of the API.
     """
-    data = {
-        "status": "OK"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
-
-
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
-def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    return jsonify({"status": "OK"})
