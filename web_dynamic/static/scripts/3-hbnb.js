9890f101bf00229106115900c10da04f627b0929
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-31 15:31:01 +0200

    Update 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   });
 
   $.get('http://localhost:5001/api/v1/status/', data => {
-    if (data.status == 'OK') {
+    if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
