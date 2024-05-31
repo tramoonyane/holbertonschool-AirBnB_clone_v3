aa719fcc4002f6171486bb6ad76da8e703fa68ac
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-31 15:29:23 +0200

    Update 100-hbnb.js

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   // Get api status
   $.get(`http://${HOST}:5001/api/v1/status/`, data => {
-    if (data.status == 'OK') {
+    if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
